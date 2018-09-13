d16869c16d8c608c83191b798589f0920b1f5e0e
Kayvan Vakili <kvakili813@gmail.com>
2018-09-13 07:35:56 +0000

    automatically backed up by learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,8 +11,12 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
+    if(this.props.IMDBRating){
+          return(<img src={imgMapper[this.props.IMDBRating]} alt={this.props.IMDBRating} />)
+        }else{
+          return(<h4>No Rating Found</h4>)
+        }
+}
 
   render() {
     return (
